2132200e43acb3240440de544932f7070b4c89f4
Aaron Spiegel <spiegela@Daigo.local>
2009-04-23 23:03:48 -0500

    Not sure how that snuck in there...

--- nomad.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-# Nomad is a program for enabling synchronous communication between
-# clustered servers over HTTP.
-#
-# Author:   Aaron Spiegel
-# License:  Distributes under the same terms as Ruby
-
-module Nomad  
-end
